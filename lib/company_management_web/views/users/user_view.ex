7862e270f097f314cbf384401f1f82243d6e5ee8
itamm15 <osinski.mateusz15@gmail.com>
2022-08-28 22:58:47 +0200

    formated naive date

--- lib/company_management_web/views/users/user_view.ex
+++ lib/company_management_web/views/users/user_view.ex
@@ -1,3 +1,7 @@
 defmodule CompanyManagementWeb.Users.UserView do
   use CompanyManagementWeb, :view
+
+  @spec format_naive_date(NaiveDateTime.t()) :: Date.t()
+  def format_naive_date(date), do: NaiveDateTime.to_date(date)
+
 end
