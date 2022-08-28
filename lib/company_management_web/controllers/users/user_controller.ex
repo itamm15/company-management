80c89842ce0e9e2313633a5b9f5049f2b6663d09
itamm15 <osinski.mateusz15@gmail.com>
2022-08-28 23:04:15 +0200

    edit user profile def

--- lib/company_management_web/controllers/users/user_controller.ex
+++ lib/company_management_web/controllers/users/user_controller.ex
@@ -3,10 +3,17 @@ defmodule CompanyManagementWeb.Users.UserController do
 
   # aliases
   alias CompanyManagement.Users
+  alias CompanyManagement.Users.User
 
   @spec index(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def index(conn, _params) do
     user_details = conn.assigns.current_user.id |> Users.get_user_by_id()
     render(conn, :index, user_details: user_details)
+  end
+
+  @spec edit(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def edit(conn, _params) do
+    changeset = User.changeset(%User{}, %{})
+    render(conn, :edit, changeset: changeset)
   end
 end
