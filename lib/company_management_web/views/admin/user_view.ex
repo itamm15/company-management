76a18776b067001590658f9ac941a05ab5e4dddd
itamm15 <osinski.mateusz15@gmail.com>
2022-09-14 00:00:29 +0200

    created links in admin user view

--- lib/company_management_web/views/admin/user_view.ex
+++ lib/company_management_web/views/admin/user_view.ex
@@ -1,3 +1,13 @@
 defmodule CompanyManagementWeb.Admin.UserView do
   use CompanyManagementWeb, :view
+
+  import Phoenix.HTML
+
+  @spec display_links(Plug.Conn.t(), String.t() | integer()) :: {:safe, binary()}
+  def display_links(conn, id) do
+    ~E"""
+      <%= link "Edit", to: Routes.admin_user_path(conn, :edit, id), class: "btn btn-primary" %>
+      <%= link "Delete", to: Routes.admin_user_path(conn, :delete, id), class: "btn btn-danger", method: :delete %>
+    """
+  end
 end
